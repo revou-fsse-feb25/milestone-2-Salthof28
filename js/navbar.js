cf17256de7ee19cef795918d8d0200e4622dacb7
Salman <salmanalthof31@gmail.com>
2025-04-12 21:40:48 +0700

    feat: add feature authentication on navbar for change title guest to username

--- js/navbar.js
+++ js/navbar.js
@@ -1,6 +1,18 @@
+import { statuslogin } from "./auth.js";
 const menu = document.getElementById('menu');
 const menulist = document.getElementById('menu-list');
-
+const accountname = document.getElementById('accountname');
+confirmstatuslogin();
+function confirmstatuslogin () {
+    const userlogin = statuslogin();
+    accountname.textContent = userlogin ? userlogin.username : "Guest";
+    // if (loginstatus) {
+    //     accountname.textContent = `${loginstatus.username}`;
+    // }
+    // else {
+    //     accountname.textContent = `Guest`;
+    // }
+}
 menu.addEventListener('click', () => {
     menulist.classList.toggle('showmenu');
     menu.classList.toggle('active');
