32888d9a47fad44024baff1f6e01a1d3919a59cf
Salman <salmanalthof31@gmail.com>
2025-04-22 08:57:12 +0700

    refactor: convert game logic to oop using class structure

--- js/number-guesing.js
+++ js/number-guesing.js
@@ -1,89 +1,112 @@
 import { getUsers ,saveUsers,statuslogin, login } from "./auth.js";
-const btnstart = document.getElementById('btnstart');
-const formguess = document.getElementById('formguess');
-const btnguess = document.getElementById('btnguess');
-let guesser = document.getElementById('guesser');
-let inptguess = document.getElementById('inptguess');
-let attempt = 5;
-let statuswin = false;
-let secretnumber;
-const users = getUsers();
-const userlogin = statuslogin();
 
-btnstart.addEventListener('click', function () {
-    formguess.style.display = 'block';
-    btnstart.style.display = 'none';
-    secretnumber = Math.floor(Math.random()*100) + 1;
-    guesser.textContent = "Guess the number I'm thinking of!";
-    console.log (secretnumber);
-})
-btnguess.addEventListener('click', processguess)
-inptguess.addEventListener('keydown', function (event) {
-    if(event.key === 'Enter'){
-        event.preventDefault(); // made click enter not refresh website
-        processguess();
+class NumberGuessingGame {
+    constructor() {
+        this.btnstart = document.getElementById('btnstart');
+        this.formguess = document.getElementById('formguess');
+        this.btnguess = document.getElementById('btnguess');
+        this.guesser = document.getElementById('guesser');
+        this.inptguess = document.getElementById('inptguess');
+
+        this.attempt = 5;
+        this.statuswin = false;
+        this.secretnumber;
+
+        this.users = getUsers();
+        this.userlogin = statuslogin();
+    }   
+    startgame () {
+        this.formguess.style.display = 'block';
+        this.btnstart.style.display = 'none';
+        this.secretnumber = Math.floor(Math.random()*100) + 1;
+        this.guesser.textContent = "Guess the number I'm thinking of!";
+        console.log (this.secretnumber);
     }
-})
-function processguess () {
-    if (inptguess.value != "") {
-        if (inptguess.value != secretnumber) {
-            if (inptguess.value > secretnumber) {
-                if (Math.abs (inptguess.value - secretnumber) <= 5) {
-                    guesser.textContent = "Lower it little more"
+    processguess (inptguess) {
+        if (inptguess != "") {
+            if (inptguess != this.secretnumber) {
+                if (inptguess > this.secretnumber) {
+                    if (Math.abs (inptguess - this.secretnumber) <= 5) {
+                        this.guesser.textContent = "Lower it little more"
+                    }
+                    else {
+                        this.guesser.textContent = "The guessed number is too big"
+                    }
                 }
                 else {
-                    guesser.textContent = "The guessed number is too big"
+                    if (Math.abs (inptguess - this.secretnumber) <= 5) {
+                        this.guesser.textContent = "Raise it little more"
+                    }
+                    else {
+                        this.guesser.textContent = "The guessed number is too low"
+                    } 
                 }
             }
             else {
-                if (Math.abs (inptguess.value - secretnumber) <= 5) {
-                    guesser.textContent = "Raise it little more"
-                }
-                else {
-                    guesser.textContent = "The guessed number is too low"
-                } 
+                this.guesser.textContent = `YOU WIN, The correct number is ${this.secretnumber}`;
+                this.statuswin = true;
+                this.savedatagame(this.statuswin)
+                this.reset ();
             }
+            inptguess = "";
+            this.chances();
         }
         else {
-            guesser.textContent = `YOU WIN, The correct number is ${secretnumber}`;
-            statuswin = true;
-            savedatagame(statuswin)
-            reset ();
+            this.guesser.textContent = "You didn't input the guessed number";
         }
+        this. inptguess.focus();
+    }
+    chances () {
+        if (this.attempt > 0) {
+            this.attempt --;
+        }
+        else {
+            this.guesser.textContent = `You Lose, the correct is ${this.secretnumber}`;
+            this.statuswin = false;
+            this.savedatagame(this.statuswin);
+            this.reset();
+        }
+        console.log(this.attempt);
+    }
+    savedatagame (statuswin) {
+        if (this.userlogin) {
+            const user = this.users.find(user => user.username == this.userlogin.username && user.password == this.userlogin.password);
+            const numberguessing = user.numberguessing;
+            statuswin ? numberguessing.win += 1 : numberguessing.lose += 1;
+            numberguessing.tg += 1;
+            saveUsers(this.users);
+            login(user);
+        }
+    }
+    reset () {
+        this.formguess.style.display = 'none';
+        this.btnstart.style.display = 'block';
+        this.btnstart.textContent = 'Play Again'
+        this.attempt = 5;
+    }
+}
+// new NumberGuessingGame();
+const numberguessinggame = new NumberGuessingGame();
+const btnstart = document.getElementById('btnstart');
+const btnguess = document.getElementById('btnguess');
+const inptguess = document.getElementById('inptguess');
+
+btnstart.addEventListener ('click', () => {
+    numberguessinggame.startgame();
+});
+
+btnguess?.addEventListener('click', () => {
+    numberguessinggame.processguess(inptguess.value);
+    inptguess.value = "";
+});
+
+inptguess?.addEventListener('keydown', (event) => {
+    if (event.key === 'Enter') {
+        event.preventDefault();
+        numberguessinggame.processguess(inptguess.value);
         inptguess.value = "";
-        chances();
     }
-    else {
-        guesser.textContent = "You didn't input the guessed number";
-    }
-    inptguess.focus();
-}
-function reset () {
-    formguess.style.display = 'none';
-    btnstart.style.display = 'block';
-    btnstart.textContent = 'Play Again'
-    attempt = 5;
-}
-function chances () {
-    if (attempt > 0) {
-        attempt --;
-    }
-    else {
-        guesser.textContent = `You Lose, the correct is ${secretnumber}`;
-        statuswin = false;
-        savedatagame(statuswin);
-        reset();
-    }
-    console.log(attempt);
-}
-// savedatagame(statuswin);
-function savedatagame (statuswin) {
-    if (userlogin) {
-        const user = users.find(user => user.username == userlogin.username && user.password == userlogin.password);
-        const numberguessing = user.numberguessing;
-        statuswin ? numberguessing.win += 1 : numberguessing.lose += 1;
-        numberguessing.tg += 1;
-        saveUsers(users);
-        login(user);
-    }
-}
+});
+// export default NumberGuessingGame;
+
+
