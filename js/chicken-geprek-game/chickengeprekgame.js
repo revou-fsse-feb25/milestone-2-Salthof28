502d8d75835c441c98fa2965558492a0b72996e8
Salman <salmanalthof31@gmail.com>
2025-04-25 02:05:10 +0700

    feat: add controller for mobile

--- js/chicken-geprek-game/chickengeprekgame.js
+++ js/chicken-geprek-game/chickengeprekgame.js
@@ -2,11 +2,21 @@ import ChickenGeprekGame from "./class-chickengeprekgame.js";
 const chickengeprekgame = new ChickenGeprekGame();
 const btnstart = chickengeprekgame.btnstart;
 const btnplayagain = document.getElementById('btnplayagain');
+const leftmove = document.getElementById('leftmove');
+const rightmove = document.getElementById('rightmove');
 
 btnstart.addEventListener('click', () => {
     chickengeprekgame.start();
-})
+});
 btnplayagain.addEventListener('click', () => {
     chickengeprekgame.start();
-})
+});
 
+leftmove.addEventListener('click', () => {
+    const key = "ArrowLeft";
+    chickengeprekgame.movechicken(key)
+});
+rightmove.addEventListener('click', () => {
+    const key = "ArrowRight"
+    chickengeprekgame.movechicken(key)
+});
